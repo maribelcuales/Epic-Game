84d306a7bf690e8ded4530df17f7855914adab3d
maribelcuales <maribelcuales@gmail.com>
2022-01-08 23:06:56 +0800

    Code added to compile and deploy smart contract.

--- scripts/run.js
+++ scripts/run.js
@@ -1,4 +1,6 @@
 const main = async () => {
   const gameContractFactory = await hre.ethers.gameContractFactory('MyEpicGame');
   const gameContract = await gameContractFactory.deploy();  
+  await gameContract.deployed();
+  console.log("Contract deployed to:", gameContract.address);
 };
