c8cadf9ef1c3d2bf7864234426b7921e999eb5f8
maribelcuales <maribelcuales@gmail.com>
2022-01-08 23:09:28 +0800

    Code added to run the smart contract.

--- scripts/run.js
+++ scripts/run.js
@@ -4,3 +4,15 @@ const main = async () => {
   await gameContract.deployed();
   console.log("Contract deployed to:", gameContract.address);
 };
+
+const runMain = async () => {
+  try {
+    await main();
+    process.exit(0);
+  } catch (error) {
+    console.log(error); 
+    process.exit(1);
+  }
+}; 
+
+runMain(); 
