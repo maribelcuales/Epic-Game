1b09180c97c23978dc95e6929cc071381b9f20da
maribelcuales <maribelcuales@gmail.com>
2022-01-11 22:29:36 +0800

    Removed comments and contract is deployed successfully.

--- scripts/run.js
+++ scripts/run.js
@@ -1,15 +1,7 @@
 const main = async () => {
-  // compile our contract and generate files needed under the artifacts directory
-  // hre stands for Hardhat Runtime Environment which contains all of the functionality of Hardhat 
-  const gameContractFactory = await hre.ethers.gameContractFactory('MyEpicGame');
-  
-  // hardhat creates a local Ethereum network then destroys it after completely running the script
-  // it will be a fresh local blockchain each time the contract is ran    
+  const gameContractFactory = await hre.ethers.getContractFactory('MyEpicGame');
   const gameContract = await gameContractFactory.deploy();
-  
-  // hardhat creates fake miners that lets our contracts be officially mined and deployed to our local blockchain 
   await gameContract.deployed();
-  // this console log runs when we are deployed giving us also the address of the deployed contract 
   console.log("Contract deployed to:", gameContract.address);
 };
 
@@ -18,9 +10,9 @@ const runMain = async () => {
     await main();
     process.exit(0);
   } catch (error) {
-    console.log(error); 
+    console.log(error);
     process.exit(1);
   }
-}; 
+};
 
-runMain(); 
+runMain();
