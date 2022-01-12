67cc54ec85c50a8333903e9dae09eaf3601bc06c
maribelcuales <maribelcuales@gmail.com>
2022-01-12 22:09:01 +0800

    Created character attributes.

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -5,6 +5,17 @@ pragma solidity ^0.8.0;
 import "hardhat/console.sol";
 
 contract MyEpicGame {
+  // Hold out character's attributes in a struct. 
+  // Add character's attributes (ex. defense, attack, etc.) 
+  struct CharacterAttributes {
+    uint characterIndex;
+    string name;
+    string imageURI;
+    uint hp;
+    uint maxHp; 
+    uint attackDamage; 
+  }
+
   constructor() {
     console.log("LET THE EPIC GAMES BEGIN!"); 
   }
