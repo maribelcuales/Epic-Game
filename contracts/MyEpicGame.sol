dcfa65653a2d6abc9af3e2dd0a3eb10f359549a2
maribelcuales <maribelcuales@gmail.com>
2022-01-12 22:17:31 +0800

    Created array of characters with their attributes.

--- contracts/MyEpicGame.sol
+++ contracts/MyEpicGame.sol
@@ -16,7 +16,20 @@ contract MyEpicGame {
     uint attackDamage; 
   }
 
-  constructor() {
+  // Create array to hold the default data of the characters 
+  // This is helpful as we mint more new characters 
+  // Array will display characters' HP, Attack, etc.  
+  CharacterAttributes[] defaultCharacters;
+
+  // Data passed in to the contract when it is created initializing the characters
+  // Pass these values in from run.js 
+  constructor(
+    string[] memory characterNames,
+    string[] memory characterImageURIs,
+    uint[] memory characterHp,
+    uint[] memory characterAttackDmg
+  ) 
+  {
     console.log("LET THE EPIC GAMES BEGIN!"); 
   }
 }
